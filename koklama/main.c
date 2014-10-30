4ca07cba7835a5799890f2e1da39494349112b1e
Pablo Prietz <github@prietz.org>
2014-10-30 14:35:35 +0200

    Initial Xcode commit

---
+++ koklama/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  koklama
+//
+//  Created by Pablo Prietz on 30.10.14.
+//  Copyright (c) 2014 Emrah Ayaz & Pablo Prietz. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+    return 0;
+}
