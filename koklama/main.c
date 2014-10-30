653a2bd69e86037dfede2a80a8a3302617f69592
Pablo Prietz <github@prietz.org>
2014-10-30 17:44:04 +0200

    Open a selected device and add filter

--- koklama/main.c
+++ koklama/main.c
@@ -17,7 +17,7 @@ int main(int argc, char *argv[])
     // look up all devices
     pcap_if_t *devlist;
 	int searchsuc = pcap_findalldevs(&devlist,errbuf);
-    
+
     // check for failure
 	if (searchsuc == -1) {
 		fprintf(stderr, "Search failure: %s\n", errbuf);
@@ -30,15 +30,15 @@ int main(int argc, char *argv[])
 		return(0);
 	}
 
-    pcap_if_t *sniffdev;
-    
+    pcap_if_t *sniffdev = NULL;
+
     bool validdev = false;
     while (!validdev) {
-        
+
         // enumerate all found devices
         uint counter = 1;
         pcap_if_t *curdev = devlist;
-        
+
         printf("Choose a device to koklama on:\n");
         while (curdev != NULL) {
             printf("\t%u) %s\n", counter,curdev->name);
@@ -49,7 +49,7 @@ int main(int argc, char *argv[])
         // read choice
         uint choosendev = 0;
         scanf("%u", &choosendev);
-        
+
         if (choosendev > 0 && choosendev < counter) {
             
             counter = 1;
@@ -69,5 +69,42 @@ int main(int argc, char *argv[])
             printf("Something went wrong while choosing a device. Please try again.\n\n");
         }
     }
+
+    struct bpf_program fp;          /* The compiled filter expression */
+    char filter_exp[] = "port 23";	/* The filter expression */
+    bpf_u_int32 mask;               /* The netmask of our sniffing device */
+    bpf_u_int32 net;                /* The IP of our sniffing device */
+
+    if (pcap_lookupnet(sniffdev->name, &net, &mask, errbuf) == -1) {
+        fprintf(stderr, "Can't get netmask for device %s\n", sniffdev->name);
+        net = 0;
+        mask = 0;
+    }
+
+    // open device
+    pcap_t *handle = pcap_open_live(sniffdev->name, BUFSIZ, 1, 1000, errbuf);
+    if (handle == NULL) {
+        fprintf(stderr, "Couldn't open device %s: %s\n", sniffdev->name, errbuf);
+        return(2);
+    }
+
+    // check if ethernet headers are available
+    if (pcap_datalink(handle) != DLT_EN10MB) {
+        fprintf(stderr, "Device %s doesn't provide Ethernet headers - not supported\n", sniffdev->name);
+        return(2);
+    }
+
+    // compile regular expression to real filter
+    if (pcap_compile(handle, &fp, filter_exp, 0, net) == -1) {
+        fprintf(stderr, "Couldn't parse filter %s: %s\n", filter_exp, pcap_geterr(handle));
+        return(2);
+    }
+
+    // apply filter
+    if (pcap_setfilter(handle, &fp) == -1) {
+        fprintf(stderr, "Couldn't install filter %s: %s\n", filter_exp, pcap_geterr(handle));
+        return(2);
+    }
+
 	return(0);
 }
