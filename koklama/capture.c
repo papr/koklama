f3f86311d90ee2f5dbcb0f7fdb20e0e67ff4e3e8
Pablo Prietz <github@prietz.org>
2014-11-02 22:00:07 +0200

    Create capture.h/c

---
+++ koklama/capture.c
@@ -0,0 +1,9 @@
+//
+//  capture.c
+//  koklama
+//
+//  Created by System Administrator on 02/11/14.
+//  Copyright (c) 2014 Emrah Ayaz & Pablo Prietz. All rights reserved.
+//
+
+#include "capture.h"
---
+++ koklama/capture.h
@@ -0,0 +1,14 @@
+//
+//  capture.h
+//  koklama
+//
+//  Created by System Administrator on 02/11/14.
+//  Copyright (c) 2014 Emrah Ayaz & Pablo Prietz. All rights reserved.
+//
+
+#ifndef __koklama__capture__
+#define __koklama__capture__
+
+#include <stdio.h>
+
+#endif /* defined(__koklama__capture__) */
