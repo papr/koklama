d380d1c932e8f95dcf57af5e14dc476a8a78c003
Pablo Prietz <github@prietz.org>
2014-12-07 20:36:30 +0200

    Start parsing packets

--- koklama/capture.c
+++ koklama/capture.c
@@ -10,5 +10,27 @@
 
 void capture_loop_cb(u_char *arg, const struct pcap_pkthdr *pkthdr, const u_char *pkt) {
         // pcap_t *handle = (pcap_t *)arg;
-        fprintf(stdout,"%d\n",pkthdr->caplen);
+        // int type = pcap_datalink(handle);, pcap_datalink_val_to_name(type)
+        // fprintf(stdout,"%d\n",pkthdr->caplen);
+
+        // radio tap header + radio tap data
+        const struct ieee80211_radiotap_header *rtphead;
+        rtphead = (struct ieee80211_radiotap_header*)pkt;
+
+        // 802.11 header
+        const u_char *frmctrlcharptr = pkt + rtphead->it_len;
+	unsigned int *frmctrlptr = (unsigned int *)frmctrlcharptr;
+        unsigned int frmctrl = *frmctrlptr; // frame control length 30bits
+        frmctrl = frmctrl >> 12;
+        frmctrl = frmctrl % 4;
+
+        if (frmctrl == 2) {
+                const u_char *addr1charptr = pkt + rtphead->it_len + 4;
+		char macStr[18];
+
+                snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
+                         addr1charptr[0], addr1charptr[1], addr1charptr[2], addr1charptr[3], addr1charptr[4], addr1charptr[5]);
+
+                fprintf(stdout,"%s\n",macStr);
+        }
 }
